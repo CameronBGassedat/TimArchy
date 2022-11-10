ea111c30a68866c65f8c013c3870a75920f43ef9
Cameron <cameron.gassedat@ynov.com>
2022-11-10 16:40:07 +0100

    [MODIFIED] websocket base for validator

---
+++ Validator/checker.js
@@ -0,0 +1,25 @@
+import {createClient} from 'redis';
+const REDIS_SERVER = "redis://127.0.0.1:6379";
+
+(async () => {
+    const client = createClient();
+    await client.connect();
+
+    client.on('ready', () => {
+            console.log("connected to localhost:${REDIS_SERVER}");
+    });
+
+    const subscriber = client.duplicate();
+    await subscriber.connect();
+
+    let commandReceived;
+    let keyReceived;
+    let valueReceived;
+
+    await subscriber.pSubscribe("keyevent@0:*", async  (message, channel) => {
+        console.log(message.toString(), channel.toString());
+        valueReceived = await client.get(message.toString());
+        keyReceived = message.toString();
+        commandReceived = channel.toString().split(':')[1]
+    }, true);
+})();
