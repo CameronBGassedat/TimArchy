aad7c8bd35d63b1abf1403cb2ce981f42fa37330
Ammooss <rouge.maxence31@gmail.com>
2022-11-25 14:05:44 +0100

    [ADD] Push branch + start gataway

--- Cloud/cloud-ws/server.js
+++ Cloud/cloud-ws/server.js
@@ -1,20 +1,42 @@
 import WebSocket,{WebSocketServer} from 'ws';
 
-
-
-//let socket = new WebSocket("ws://127.0.0.1:3000")
 const WEB_SOCKET_PORT = 3000;
-
 const server = new WebSocketServer({ port : WEB_SOCKET_PORT });
+
+const promClient = require('prom-client');
+
+// const collectMetrics = promClient.collectDefaultMetrics();
+// const Registry = promClient.Registry;
+// const register = new Registry();
+// collectMetrics({register});
+// const gw = require('prom-pushgateway');
+
+const gw = require('prom-pushgateway').createServer({
+  port: 39110,
+    readPromMetrics: () => promClient.register.metrics()
+}, (err) => {
+  // listening for /metrics requests
+})
+
 
 
 server.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
+    ws.on('message', function message(event) {
+        // CODE ICI
+        console.log('received: %s', event.data);
+
+        // Data push -> PushGateway -> pull Prometheus
+        gw.gateway.ingestMetrics(
+          // add custom metrics to the /metrics report
+          event.data
+        ), (err) => {
+          // metrics will be part of the next /metrics report
+        }
       });
     console.log("New connection established");
 
 });
+
 
 
 console.log("WebSocket server started at ws://locahost:"+ WEB_SOCKET_PORT);
