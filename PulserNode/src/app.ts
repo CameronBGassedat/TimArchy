293af8a689f50a2dacb86c0929358425a436f902
Cameron <cameron.gassedat@ynov.com>
2022-12-01 10:22:31 +0100

    [ADD] new json data for pulser

--- PulserNode/src/app.ts
+++ PulserNode/src/app.ts
@@ -1,6 +1,8 @@
 import { createClient } from 'redis';
+import dataJson from '../../Resources/allsensor.json';
 
 const url = process.env.REDIS_URL || 'redis://localhost:6379';
+
 
 main();
 async function main(){
@@ -10,39 +12,37 @@ async function main(){
 
   client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
-  
-  var mykey : number = 0;
-  let lightdata = new Light("kitchen light", false);
-  const jsonbject = JSON.stringify(lightdata);
-  
-  while (true) {
-    await delay(1000);
 
-    await client.set(mykey.toString(), jsonbject);
+  var mykey : number = 0;
+  let jsonArray : Array<string>;
+  for (var singledata in dataJson)
+  {
+    delay(1000);
+    await client.set(mykey.toString(), JSON.stringify(singledata));
     mykey++;
   }
-}
-
-class Light {
-  name : string;
-  status : boolean;
-  
-  constructor(name: string, status : boolean) {
-      this.name = name;
-      this.status = status;
-  }
-}
-
-class Data {
-  timestamp : Date;
-  object : Light;
-
-  constructor(timestamp: Date, object : Light) {
-    this.timestamp = timestamp;
-    this.object = object;
-}
 }
 
 function delay(ms: number) {
   return new Promise( resolve => setTimeout(resolve, ms) );
 }
+
+// class Light {
+//   name : string;
+//   status : boolean;
+  
+//   constructor(name: string, status : boolean) {
+//       this.name = name;
+//       this.status = status;
+//   }
+// }
+
+// class Data {
+//   timestamp : Date;
+//   object : Light;
+
+//   constructor(timestamp: Date, object : Light) {
+//     this.timestamp = timestamp;
+//     this.object = object;
+//   }
+// }
