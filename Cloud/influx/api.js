924bc0ea2381f0ce012310986ccd63cda42fcef6
Cameron <cameron.gassedat@ynov.com>
2023-01-12 11:12:09 +0100

    feat (API) get all on influx database

--- Cloud/influx/api.js
+++ Cloud/influx/api.js
@@ -1,29 +1,58 @@
 import {InfluxDB, Point} from '@influxdata/influxdb-client'
+import redis,{createClient} from 'redis';
 import {hostname} from 'node:os'
 
-const token = "THISISMYTOKENAPI"
-const url = 'http://influxdb:8086'
-let org = `TimArchy`
-let bucket = `DATA`
+/* CLOUD */
+const CloudToken = "THISISMYTOKENAPI"
+const CloudUrl = 'http://influxdb:8086'
+let CloudOrg = `TimArchy`
+let CloudBucket = `DATA`
+const CLOUD_WEB_SOCKET_PORT = 3000;
+const CloudServer = new WebSocketServer({ port : WEB_SOCKET_PORT });
+const CloudClient = new InfluxDB({CloudUrl, CloudToken}).getQueryApi(CloudOrg)
 
-const WEB_SOCKET_PORT = 3000;
-const server = new WebSocketServer({ port : WEB_SOCKET_PORT });
-const client = new InfluxDB({url, token})
+/* LOCAL */
+const LocalUrl = 'redis://redis'
+const LocalPort = 6379
+const LocalClient = redis.createClient({
+  url: LocalUrl,
+  port: LocalPort
+});
 
+/* HTTP Calls */
 const Http = new XMLHttpRequest();
-Http.open("GET", url);
-Http.send();
-Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
-  }
+
+Http.open("GET", GetAll());
+
+function GetAll() {
   
-function create()  {
-    let writeClient = client.getWriteApi(org, bucket)
-    writeClient.useDefaultTags({location: hostname()})
-    let msg = JSON.parse(data)
+  /* Cloud */
+  const fluxQuery = 'from(bucket:' +  CloudBucket + ')';
+  const observer = {
+    next(row, tableMeta) {
+      const o = tableMeta.toObject(row)
+      // console.log(
+      //   `${o._value} ${o._measurement} in '${o.location}' (${o.sensor_id}): ${o._field}=${o._value}`
+      // )
+    }
+  }
+  CloudClient.queryRows(fluxQuery, observer)
 }
 
-function read() {}
+// Http.onreadystatechange = (e) => {
+//   console.log(Http.responseText)
+// }
+
+// Http.send();
+  
+// function create()  {
+//     let writeClient = client.getWriteApi(org, bucket)
+//     writeClient.useDefaultTags({location: hostname()})
+//     let msg = JSON.parse(data)
+// }
+
+function get() {}
+
 
 function update() {}
 
